79c3660ac52c8f2cae7354465ff84ce1e1ef232e
tonetheman <tonetheman@gmail.com>
2018-12-26 15:17:32 -0600

    changes to python script and 8 and 16

--- make_colors.py
+++ make_colors.py
@@ -1,10 +1,16 @@
 
-import os
-data = open("color_list.txt","r").readlines()
 
-for line in data:
-    l = line.split()
-    if len(l)>0:
-        cmd = "magick -size 32x32 xc:{0} {0}.png".format(l[0])
-        print(cmd)
-        res = os.system(cmd)
+def makepng(size):
+        import os
+        data = open("color_list.txt","r").readlines()
+
+        for line in data:
+                l = line.split()
+                if len(l)>0:
+                        cmd = "magick -size {1} xc:{0} {0}.png".format(l[0],size)
+                        print(cmd)
+                        res = os.system(cmd)
+
+# makepng("8x8")
+# makepng("32x32")
+makepng("16x16")
